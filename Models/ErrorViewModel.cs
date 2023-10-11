de64a82ac7180711fbec5cab8c3fde4f542d501f
Domenica Escobar <domenica.escobar.moyano@udla.edu.ec>
2023-10-11 07:15:38 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DE_Ejercicio1CF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
